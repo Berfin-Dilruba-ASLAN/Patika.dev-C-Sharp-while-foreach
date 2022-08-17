6c460335815b61062140f8e222f7a418b2ca7969
Berfin Dilruba ASLAN <98692629+Berfin-Dilruba-ASLAN@users.noreply.github.com>
2022-08-17 11:38:39 +0300

    Add files via upload

---
+++ while foreach/while_foreach/while_foreach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char character = 'a';
+            while(character < 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+            string[] arabalar = { " ", "Mini Cooper", "BMW", "Ford", "Nissan" };
+            foreach(var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
